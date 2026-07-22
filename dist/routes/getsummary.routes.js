98f3f9bfeb2977319a2ddbb9056849049c92def5
Narvin Patidar <narvin.patidar@classplus.co>
2026-07-22 23:04:28 +0530

    updated

--- dist/routes/getsummary.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const getSummary_1 = require("../controllers/getSummary");
-const router = (0, express_1.Router)();
-// Route to calculate quiz scores for a given session_id
-router.post("/calculate-quiz-scores/:session_id", getSummary_1.calculateQuizScores);
-exports.default = router;
